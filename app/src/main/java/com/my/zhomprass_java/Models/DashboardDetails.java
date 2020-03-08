fe29e7dab37d21aef35b7ab1c505bba2966adb6b
abmotin <motiniu92@gmail.com>
2020-03-08 10:44:19 +0600

    Dashboard Details Model

---
+++ app/src/main/java/com/my/zhomprass_java/Models/DashboardDetails.java
@@ -0,0 +1,34 @@
+package com.my.zhomprass_java.Models;
+
+public class DashboardDetails {
+    private int id;
+    private String date;
+    private String from_user_name;
+    private int balance;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getFrom_user_name() {
+        return from_user_name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public DashboardDetails(int id, String date, String from_user_name, int balance) {
+        this.id = id;
+        this.date = date;
+        this.from_user_name = from_user_name;
+        this.balance = balance;
+    }
+
+    public DashboardDetails() {
+    }
+}
